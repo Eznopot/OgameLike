a7ddb3e1e69c3e18c14596573ce4614430c7243a
Romain Estrada <62434967+Adartse-r@users.noreply.github.com>
2022-01-04 18:37:02 +0100

    Modif

--- SymfonyProject/src/Controller/LoginController.php
+++ SymfonyProject/src/Controller/LoginController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+/**
+ * @Route("/connexion")
+ */
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function __construct()
     {
+
+    }
+
+    /**
+     * @Route("/login", name="connexion_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'error' => $error
         ]);
     }
 }
