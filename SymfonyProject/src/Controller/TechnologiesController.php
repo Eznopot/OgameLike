41e3cb235c25a5a2f527d5aa94ba1b7b29d97c9c
mano.baffie <mano.baffie@epitech.eu>
2022-01-07 03:24:00 +0100

    clean v1 all work on techno and buy building

--- SymfonyProject/src/Controller/TechnologiesController.php
+++ SymfonyProject/src/Controller/TechnologiesController.php
@@ -3,76 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Technologies;
 
 class TechnologiesController extends AbstractController
 {
     #[Route('/technologies/ajax', name: 'technologies_ajax')]
     public function ajax(Request $request) {
-        $technologies = $this->getDoctrine()
-            ->getRepository(Technologies::class)
-            ->findAll();
-
-        if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
-            $jsonData = array();
-            $idx = 0;
-            foreach($technologies as $technology) {
-                $temp = array(
-                    'technology' => $technology,
-                );
-                $jsonData[$idx++] = $temp;
-            } 
-            return new JsonResponse($jsonData);
-        }
-        else {
-            return $this->render('technologies/index.html.twig');
-        }
+        dump($request);
     }
 
     #[Route('/technologies', name: 'technologies')]
-    // public function index(ManagerRegistry $doctrine): Response
     public function index(): Response
     {
-        // $technologies = $doctrine->getRepository(Technology::class)->findAll();
-        // $ownedTechnologies = $doctrine->getRepository(Technology::class)->find($this->getUser()->getId());
 
-        $goldPlayer = 10;
+        $request = Request::createFromGlobals();
+        $upgradeId = $request->request->get('type');
 
-        $technolgiesStats = [
-            [
-                'price' => 10,
-                'stats' => '',
-                'lvl' => 3,
+        if ($upgradeId !== null) {
+            dump($upgradeId);
+        }
 
-                'upgradeTime' => 5,
-                'actualUpgradeTime' => 2,
+        $technologies = $this->getDoctrine()->getRepository(Technologies::class)->findAll();
 
-                'name' => 'def',
-                'explaine' => 'this technology is for upgrade the unites damage',
-                'explaineStats' => 'all your unites got +5 dmg'
-            ],
-            [
-                'upgradeTime' => 5,
-                'actualUpgradeTime' => 1,
-
-                'name' => 'attaque',
-                'explaine' => 'this technology is for upgrade the build damage',
-                'explaineStats' => 'all your build got +5 dmg'
-            ],
-            [
-                'upgradeTime' => 5,
-                'actualUpgradeTime' => 4,
-
-                'name' => 'trest2',
-                'explaine' => 'qeds',
-                'explaineStats' => 'dqdq'
-            ]
-        ];
 
         return $this->render('technologies/index.html.twig', [
-            'goldPlayer' => $goldPlayer,
-            'technolgiesStats' => $technolgiesStats,
+            'technolgiesStats' => $technologies
         ]);
     }
 }
--- SymfonyProject/src/DataFixtures/Recycler.php
+++ SymfonyProject/src/DataFixtures/Recycler.php
@@ -4,14 +4,69 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-
+use App\Entity\Technologies;
+use App\Entity\User;
+use App\Entity\Batiments;
 
 class Recycler extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $technoAttack = new Technologies();
+
+        $technoAttack->setName("Attack")
+                ->setPrice(10)
+                ->setLvlMax(10)
+                ->setDescription("this upgrade is use for upgrade the attack of your unites")
+                ->setDamage(0)
+                ->setGoldBoost(0)
+                ->setDamagePerLevel(5)
+                ->setGoldBoostPerLevel(0)
+                ->setUpgradeTime(10);
+        $manager->persist($technoAttack);
+
+        $technoGold = new Technologies();
+        $technoGold->setName("Gold boost")
+                ->setPrice(40)
+                ->setLvlMax(5)
+                ->setDescription("this upgrade is use for upgrade the gold product by building")
+                ->setDamage(0)
+                ->setGoldBoost(0)
+                ->setDamagePerLevel(0)
+                ->setGoldBoostPerLevel(10)
+                ->setUpgradeTime(10);
+        $manager->persist($technoGold);
+
+        for ($i=0; $i < 4; $i++) {
+            $user = new User();
+            $user->setUsername('user_num_'.$i)
+                ->setPassword('1234')
+                ->setGold(0)
+                ->setElo(0)
+                ->setImage("https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg");
+            $manager->persist($user);
+        }
+
+        $type = ["damage", "unite", "gold"];
+        for ($i=0; $i < 10; $i++) {
+            $rand = rand(0, 2);
+            $build = new Batiments();
+            $build->setName("build_".$type[$rand].$i)
+                ->setPrice(rand(10, 15))
+                ->setLvlMax(10)
+                ->setImage("https://thumbs.dreamstime.com/b/buildings-line-icon-city-architecture-sign-skyscraper-building-vector-buildings-line-icon-city-architecture-sign-skyscraper-148085776.jpg")
+
+                ->setDamage($type[$rand] == "damage" ? rand(100, 200) : 0)
+                ->setLevel(0)
+                ->setHp(rand(500, 900))
+
+                ->setDamagePerLvl($type[$rand] == "damage" ? rand(20, 50) : 0)
+                ->setGoldPerHour($type[$rand] == "gold" ? rand(10, 50) : 0)
+                ->setHpPerLvl(rand(100, 300))
+
+                ->setGoldPerHourPerLvl($type[$rand] == "damage" ? rand(100, 200) : 0);
+            $manager->persist($build);    
+        }
 
         $manager->flush();
     }
