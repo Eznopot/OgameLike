c446f9add1eb56c7c2bac22389ead6316f46de4c
Remy <remy12.salem@epitech.eu>
2022-01-07 22:31:51 +0100

    beginning of Homepage

--- SymfonyProject/src/Controller/HomeController.php
+++ SymfonyProject/src/Controller/HomeController.php
@@ -11,9 +11,22 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $technoOwned = $user->getUserTechnoOwned();
+        $batOwned = $user->getBatimentsOwned();
+        $arrayOfTechno = array();
+        foreach ($technoOwned as $elem) {
+            array_push($arrayOfTechno, $elem->getType());
+        }
+        $arrayOfBat = array();
+        foreach ($batOwned as $elem) {
+            array_push($arrayOfBat, $elem->getType());
+        }
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'technos_owned' => $arrayOfTechno,
+            'batiments_owned' => $arrayOfBat
         ]);
     }
 }
