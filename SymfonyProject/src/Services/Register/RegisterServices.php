5cc0489d93cd5f6d9ae74356a6c1f4a2400c720b
mano.baffie <mano.baffie@epitech.eu>
2022-01-11 12:58:52 +0100

    a

--- SymfonyProject/src/Services/Register/RegisterServices.php
+++ SymfonyProject/src/Services/Register/RegisterServices.php
@@ -32,21 +32,28 @@ class RegisterServices
         if ($check) {
             return 1;
         } else {
+            dump($form->get('Planet'));
+
+            $planetChoose = $doctrine->getRepository(Planets::class)->find($form->get('Planet'));
+
+            dump($planetChoose);
+
             $user = new User();
             $user->setUsername($form->get('Email')->getData());
             $hash = $encoder->encodePassword($user, $form->get('Password')->getData());
-            $user->setPassword($hash);
-            $user->setGold(400);
-            $user->setUnits(150);
-            $user->setElo(1000);
-            $user->setPlanet($doctrine->getRepository(Planets::class)->find($form->get('Planet')->getData()));
-            $user->setLastUpdate(new \DateTime());
+
+            $user->setPassword($hash)
+                ->setGold(400)
+                ->setUnits(150)
+                ->setElo(1000)
+                ->setPlanet()
+                ->setLastUpdate(new \DateTime());
 
             $techno = $doctrine->getRepository(Planets::class)->findAll();
 
-            for ($i=0; $i < $techno->count(); $i++) {
+            for ($i=0; $i < count($techno); $i++) {
                 $technologiesOwned = new technologiesOwned();
-                $technologiesOwned->setType($technologiesOwned)
+                $technologiesOwned->setType($techno[$i])
                             ->setLevel(0)
                             ->setStartupgrade(null)
                             ->setEndupgrade(null)
